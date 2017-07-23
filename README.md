007e38aa9f60242a88bd572b8311ac1f35ff165d
alifadhli <mr_86_ha_mo@yahoo.com>
2017-07-23 09:41:58 -0500

    Friend Finder
    Friend Finder

---
+++ README.md
@@ -0,0 +1,5 @@
+# Friend Finder - Node and Express Servers
+
+### Compatibility based application
+
+* The application will take in results from users' surveys then compare their results against all other users who have completed it. 
---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+
+var friends = [
+   {
+      "name":"Lynn Johnson",
+      "photo":"http://www2.pictures.zimbio.com/gi/Fresh+Air+Funds+Salute+American+Heroes+QW8zvTnKkBDl.jpg",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+      "name":"John Paul",
+      "photo":"http://d3i6fh83elv35t.cloudfront.net/newshour/wp-content/uploads/2014/04/John-Paul-II-Final.jpg",
+      "scores":[4,2,5,1,3,2,2,1,3,2]
+   },
+   {
+      "name":"Rose Al-jumaily",
+      "photo":"http://scontent.cdninstagram.com/t51.2885-15/s480x480/e15/10537265_675877039172236_1090617626_n.jpg?ig_cache_key=NzY1MTU2NDcyOTQwMDU5MjI5.2",
+      "scores":[5,2,2,2,4,1,3,2,5,5]
+   },
+   {
+      "name":"Sam Smith",
+      "photo":"http://assets.gcstatic.com/u/apps/asset_manager/uploaded/2014/37/sam-smith-press-shot-2014-2-1410971393-custom-2.jpg",
+      "scores":[3,3,4,2,2,1,3,2,2,3]
+   },
+	{
+	   "name":"Claire Hanna",
+	   "photo":"https://shawglobalnews.files.wordpress.com/2016/12/claire-hanna-december-2016.jpg?quality=70&strip=all&w=220&h=260&crop=1",
+	   "scores":[4,3,4,1,5,2,5,3,1,4]
+	},
+      {
+      "name":"Salem Sabri",
+      "photo":"https://s-media-cache-ak0.pinimg.com/736x/2f/26/f6/2f26f6ec25e771b6ba3ddd40b07022db--tamzin-merchant-season-.jpg",
+      "scores":[3,3,4,1,4,3,5,3,5,4]
+   },
+	{
+	   "name":"Adele Alawi",
+	   "photo":"https://pbs.twimg.com/media/CPy9duFUcAA0GGR.jpg",
+	   "scores":[4,4,2,3,2,2,3,2,4,5]
+	}
+
+]
+
+module.exports = friends;
